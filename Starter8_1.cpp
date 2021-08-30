948eb427e972af1d5a1b192c285cf4768282fc56
Ankit Ranjan <ranjanankit1313@gmail.com>
2021-08-30 07:05:57 +0530

    Starter8 1

---
+++ Starter8_1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define input freopen("input.txt", "r", stdin)
+#define output freopen("output.txt","w",stdout)
+using namespace std;
+int main()
+{ 
+    fast;
+    int n;
+    cin>>n;
+    int res = n*(n/2);
+    cout<<res<<endl;
+}
